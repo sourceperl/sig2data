a49303641d4f0d9ea36a4ec1334d843597dcc911
sourceperl <loic.celine@free.fr>
2013-11-06 17:51:33 +0100

    add check if data already in DB (for InnoDB) on callback PHP sensor.php

--- callback/sensor.php
+++ callback/sensor.php
@@ -19,10 +19,14 @@ $appid  = stripcslashes($_POST['appid']);
 $appkey  = stripcslashes($_POST['appkey']);
 
 // check the app key
-$result = $mysqli->query("SELECT id FROM `vars` WHERE var_name='callback_app_key' AND var='".$appkey."';");
+$result = $mysqli->query("SELECT id FROM `vars` ".
+                         "WHERE var_name='callback_app_key' AND var='".
+                         $appkey."';");
 if ($result->num_rows == 0) {
   // log error and exit script
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, NOW(), 'err: app_key not valid, connect from IP: ".$stat_ip."/".$ua."');");
+  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, ".
+                 "NOW(), 'err: app_key not valid, connect from IP: ".$stat_ip.
+                 "/".$ua."');");
   exit();
 }
 
@@ -30,10 +34,14 @@ if ($result->num_rows == 0) {
 $data = json_decode($cb_msg);
 // check json decoding
 if ($data === null) {
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, NOW(), 'err: json_decode return null json: ".$cb_msg."');");
+  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+                 "VALUES (NULL, NOW(), 'err: json_decode return null json: ".
+                 $cb_msg."');");
   exit();
 } else {
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, NOW(), 'log: json_decode ok');");
+  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+                 "VALUES (NULL, NOW(), 'log: json_decode ok json: ".
+                 $cb_msg."');");
 }
 
 // JSON params
@@ -45,14 +53,27 @@ $station_id  = isset($data->{'msg'}->{'station'}) ? $data->{'msg'}->{'station'} 
 $station_lvl = isset($data->{'msg'}->{'lvl'}) ? $data->{'msg'}->{'lvl'} : 0;
 
 // get object_id
-$result = $mysqli->query("SELECT object_id FROM `objects` WHERE modem_id='".$uid."';");
+$result = $mysqli->query("SELECT object_id FROM `objects` WHERE modem_id='".
+                          $uid."';");
 if ($result->num_rows == 0) {
   // log error and exit script
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, NOW(), 'err: unknown object uid: ".$uid."');");
+  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+                 "VALUES (NULL, NOW(), 'err: unknown object uid: ".$uid."');");
   exit();
 }
 $row = $result->fetch_assoc();
 $obj_id = $row['object_id'];
+
+// check if data is already in DB
+$result = $mysqli->query("SELECT message_id FROM `message` WHERE object_id='".
+                         $obj_id."' AND rx_timestamp='".$timestamp."';");
+if ($result->num_rows == 0) {
+  // log error and exit script
+  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, ".
+                 "NOW(), 'err: msg already in DB for obj ID/timestamp".
+                 $obj_id."/".$timestamp.");");
+  exit();
+}
 
 // write to DB
 $sql = "INSERT INTO `messages` (`message_id`, `object_id`, "
