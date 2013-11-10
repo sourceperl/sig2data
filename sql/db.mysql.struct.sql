14ba1925136ef5d52179e679e863987b6c3ba5c1
sourceperl <loic.celine@free.fr>
2013-11-10 15:56:58 +0100

    update DB struct

--- sql/db.mysql.struct.sql
+++ sql/db.mysql.struct.sql
@@ -3,17 +3,47 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mar 05 Novembre 2013 à 10:29
+-- Généré le: Dim 10 Novembre 2013 à 15:51
 -- Version du serveur: 5.5.31
 -- Version de PHP: 5.4.4-14+deb7u5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Base de données: `beedb`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `debug_log`
+--
+
+CREATE TABLE IF NOT EXISTS `debug_log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Doublure de structure pour la vue `last_view`
+--
+CREATE TABLE IF NOT EXISTS `last_view` (
+`last_view` datetime
+,`object_id` int(10) unsigned
+,`modem_id` varchar(20)
+);
 -- --------------------------------------------------------
 
 --
@@ -24,12 +54,14 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `message_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned DEFAULT NULL,
   `rx_timestamp` bigint(20) unsigned DEFAULT NULL,
-  `type` varchar(8) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
   `payload` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `station_id` int(10) unsigned DEFAULT NULL,
   `station_lvl` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`message_id`),
-  UNIQUE KEY `object_id` (`object_id`,`rx_timestamp`)
+  UNIQUE KEY `msg_unique` (`object_id`,`rx_timestamp`),
+  KEY `object_id` (`object_id`),
+  KEY `rx_timestamp` (`rx_timestamp`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -61,7 +93,9 @@ CREATE TABLE IF NOT EXISTS `sig_index` (
   `index_p` int(10) unsigned DEFAULT NULL,
   `index_n` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `object_id` (`object_id`,`var_id`,`rx_timestamp`)
+  UNIQUE KEY `unique_var` (`object_id`,`var_id`,`rx_timestamp`),
+  KEY `object_id` (`object_id`),
+  KEY `rx_timestamp` (`rx_timestamp`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -92,7 +126,19 @@ CREATE TABLE IF NOT EXISTS `sig_stat` (
 CREATE TABLE IF NOT EXISTS `vars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `var_name` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `var` int(11) DEFAULT NULL,
+  `var` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la vue `last_view`
+--
+DROP TABLE IF EXISTS `last_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `last_view` AS select from_unixtime((max(`messages`.`rx_timestamp`) / 1000)) AS `last_view`,`messages`.`object_id` AS `object_id`,hex(`objects`.`modem_id`) AS `modem_id` from (`messages` join `objects`) where (`objects`.`object_id` = `messages`.`object_id`) group by `messages`.`object_id` limit 100;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
