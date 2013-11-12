20cbbc40de024a6508a00a2b3daba6d5fdccb06f
sourceperl <loic.celine@free.fr>
2013-11-12 16:46:07 +0100

    new table names in DB + some minor changes

--- callback/sensor.php
+++ callback/sensor.php
@@ -4,6 +4,7 @@ $mysqli = new mysqli('localhost','bee_sigfox_cbk', '', 'beedb');
 // check DB connect
 if ($mysqli->connect_errno) {
   printf("Connect failed: %s\n", $mysqli->connect_error);
+  http_response_code(500);
   exit();
 }
 // turn on auto-commit
@@ -19,15 +20,16 @@ $appid  = isset($_POST['appid'])    ? stripcslashes($_POST['appid'])    : "";
 $appkey = isset($_POST['appkey'])   ? stripcslashes($_POST['appkey'])   : "";
 
 // check the app key
-$result = $mysqli->query("SELECT id FROM `vars` ".
+$result = $mysqli->query("SELECT id FROM `sig_vars` ".
                          "WHERE var_name='callback_app_key' AND var='".
                          $appkey."';");
 if ($result->num_rows == 0) {
   // log error and exit script
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) VALUES (NULL, ".
+  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) VALUES (NULL, ".
                  "NOW(), 'err: app_key not valid, connect from IP: ".$stat_ip.
                  "/".$ua."');");
   $mysqli->close();
+  http_response_code(500);
   exit();
 }
 
@@ -35,31 +37,37 @@ if ($result->num_rows == 0) {
 $data = json_decode($cb_msg);
 // check json decoding
 if ($data === null) {
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
                  "VALUES (NULL, NOW(), 'err: json_decode return null json: ".
                  $cb_msg."');");
   $mysqli->close();
+  http_response_code(500); 
   exit();
 } else {
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
                  "VALUES (NULL, NOW(), 'log: json_decode ok json: ".
                  $cb_msg."');");
 }
 
 // JSON params
 $uid         = hexdec($data->{'msg'}->{'ctxt'}->{'uid'});
-$timestamp   = $data->{'msg'}->{'when'};
-$msg_type    = $data->{'msg'}->{'type'};
-$msg_payload = $data->{'msg'}->{'payload'};
-$station_id  = isset($data->{'msg'}->{'station'}) ? $data->{'msg'}->{'station'} : 0;
-$station_lvl = isset($data->{'msg'}->{'lvl'}) ? $data->{'msg'}->{'lvl'} : 0;
+$timestamp   = isset($data->{'msg'}->{'received'})
+               ? $data->{'msg'}->{'received'}/1000 : "";
+$msg_type    = isset($data->{'msg'}->{'type'}) 
+               ? $data->{'msg'}->{'type'}          : "";
+$msg_payload = isset($data->{'msg'}->{'payload'})
+               ? $data->{'msg'}->{'payload'}       : "";
+$station_id  = isset($data->{'msg'}->{'station'})
+               ? $data->{'msg'}->{'station'}       : 0;
+$station_lvl = isset($data->{'msg'}->{'lvl'})
+               ? $data->{'msg'}->{'lvl'}           : 0;
 
 // get object_id
-$result = $mysqli->query("SELECT object_id FROM `objects` WHERE modem_id='".
+$result = $mysqli->query("SELECT object_id FROM `sig_objects` WHERE modem_id='".
                           $uid."';");
 if ($result->num_rows == 0) {
   // log error and exit script
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) ".
+  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
                  "VALUES (NULL, NOW(), 'err: unknown object uid: ".$uid."');");
   $mysqli->close();
   exit();
@@ -68,17 +76,17 @@ $row = $result->fetch_assoc();
 $obj_id = $row['object_id'];
 
 // insert in DB, if not already exist (if polling value or duplicate callback)
-$sql = "INSERT INTO `messages` (`message_id`, `object_id`, "
+$sql = "INSERT INTO `sig_messages` (`message_id`, `object_id`, "
        ."`rx_timestamp`, `type`, `payload`, `station_id`, `station_lvl`) "
        ."SELECT NULL, '".$obj_id."', '".$timestamp."', '".$msg_type."', '"
        .$msg_payload."', '".$station_id."', '".$station_lvl."' FROM DUAL "
-       ."WHERE NOT EXISTS (SELECT * FROM `messages` WHERE `object_id` = '"
+       ."WHERE NOT EXISTS (SELECT * FROM `sig_messages` WHERE `object_id` = '"
        .$obj_id."' AND `rx_timestamp`= '".$timestamp."' LIMIT 1);";
 $mysqli->query($sql);
 // check if insert or not (= duplicate)
 if ($mysqli->affected_rows == 0) {
   // log warning
-  $mysqli->query("INSERT INTO debug_log (`id`, `date`, `text`) "
+  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) "
                  ."VALUES (NULL, NOW(), "
                  ."'warn: callback duplicate obj_id/timestamp: "
                  .$obj_id."/".$timestamp."');");
--- sql/db.mysql.struct.sql
+++ sql/db.mysql.struct.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Dim 10 Novembre 2013 à 15:51
+-- Généré le: Mar 12 Novembre 2013 à 16:43
 -- Version du serveur: 5.5.31
 -- Version de PHP: 5.4.4-14+deb7u5
 
@@ -23,10 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `debug_log`
+-- Structure de la table `sig_debug_log`
 --
 
-CREATE TABLE IF NOT EXISTS `debug_log` (
+CREATE TABLE IF NOT EXISTS `sig_debug_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
   `text` text COLLATE utf8_unicode_ci NOT NULL,
@@ -37,23 +37,32 @@ CREATE TABLE IF NOT EXISTS `debug_log` (
 -- --------------------------------------------------------
 
 --
--- Doublure de structure pour la vue `last_view`
+-- Structure de la table `sig_index`
 --
-CREATE TABLE IF NOT EXISTS `last_view` (
-`last_view` datetime
-,`object_id` int(10) unsigned
-,`modem_id` varchar(20)
-);
+
+CREATE TABLE IF NOT EXISTS `sig_index` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `object_id` int(10) unsigned DEFAULT NULL,
+  `var_id` int(10) unsigned DEFAULT NULL,
+  `rx_timestamp` bigint(20) unsigned DEFAULT NULL COMMENT 'epoch unix (in UTC time)',
+  `index_p` int(10) unsigned DEFAULT NULL,
+  `index_n` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_var` (`object_id`,`var_id`,`rx_timestamp`),
+  KEY `object_id` (`object_id`),
+  KEY `rx_timestamp` (`rx_timestamp`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `messages`
+-- Structure de la table `sig_messages`
 --
 
-CREATE TABLE IF NOT EXISTS `messages` (
+CREATE TABLE IF NOT EXISTS `sig_messages` (
   `message_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned DEFAULT NULL,
-  `rx_timestamp` bigint(20) unsigned DEFAULT NULL,
+  `rx_timestamp` bigint(20) unsigned DEFAULT NULL COMMENT 'epoch unix (in UTC time)',
   `type` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
   `payload` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `station_id` int(10) unsigned DEFAULT NULL,
@@ -67,10 +76,10 @@ CREATE TABLE IF NOT EXISTS `messages` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `objects`
+-- Structure de la table `sig_objects`
 --
 
-CREATE TABLE IF NOT EXISTS `objects` (
+CREATE TABLE IF NOT EXISTS `sig_objects` (
   `object_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `modem_id` int(10) unsigned DEFAULT NULL,
   `modem_key` int(10) unsigned DEFAULT NULL,
@@ -82,22 +91,13 @@ CREATE TABLE IF NOT EXISTS `objects` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `sig_index`
+-- Doublure de structure pour la vue `sig_obj_last_view`
 --
-
-CREATE TABLE IF NOT EXISTS `sig_index` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `object_id` int(10) unsigned DEFAULT NULL,
-  `var_id` int(10) unsigned DEFAULT NULL,
-  `rx_timestamp` bigint(20) unsigned DEFAULT NULL,
-  `index_p` int(10) unsigned DEFAULT NULL,
-  `index_n` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_var` (`object_id`,`var_id`,`rx_timestamp`),
-  KEY `object_id` (`object_id`),
-  KEY `rx_timestamp` (`rx_timestamp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+CREATE TABLE IF NOT EXISTS `sig_obj_last_view` (
+`last_view` datetime
+,`object_id` int(10) unsigned
+,`modem_id` varchar(20)
+);
 -- --------------------------------------------------------
 
 --
@@ -108,22 +108,23 @@ CREATE TABLE IF NOT EXISTS `sig_stat` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `object_id` int(10) unsigned DEFAULT NULL,
   `var_id` int(10) unsigned DEFAULT NULL,
-  `rx_timestamp` int(10) unsigned DEFAULT NULL,
+  `rx_timestamp` int(10) unsigned DEFAULT NULL COMMENT 'epoch unix (in UTC time)',
   `var_min` int(11) DEFAULT NULL,
   `var_avg` int(11) DEFAULT NULL,
   `var_max` int(11) DEFAULT NULL,
   `var_inst` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `object_id` (`object_id`,`var_id`,`rx_timestamp`)
+  UNIQUE KEY `object_id` (`object_id`,`var_id`,`rx_timestamp`),
+  KEY `rx_timestamp` (`rx_timestamp`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `vars`
+-- Structure de la table `sig_vars`
 --
 
-CREATE TABLE IF NOT EXISTS `vars` (
+CREATE TABLE IF NOT EXISTS `sig_vars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `var_name` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `var` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -133,11 +134,11 @@ CREATE TABLE IF NOT EXISTS `vars` (
 -- --------------------------------------------------------
 
 --
--- Structure de la vue `last_view`
+-- Structure de la vue `sig_obj_last_view`
 --
-DROP TABLE IF EXISTS `last_view`;
+DROP TABLE IF EXISTS `sig_obj_last_view`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `last_view` AS select from_unixtime((max(`messages`.`rx_timestamp`) / 1000)) AS `last_view`,`messages`.`object_id` AS `object_id`,hex(`objects`.`modem_id`) AS `modem_id` from (`messages` join `objects`) where (`objects`.`object_id` = `messages`.`object_id`) group by `messages`.`object_id` limit 100;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `sig_obj_last_view` AS select from_unixtime(max(`sig_messages`.`rx_timestamp`)) AS `last_view`,`sig_messages`.`object_id` AS `object_id`,hex(`sig_objects`.`modem_id`) AS `modem_id` from (`sig_messages` join `sig_objects`) where (`sig_objects`.`object_id` = `sig_messages`.`object_id`) group by `sig_messages`.`object_id` limit 100;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
