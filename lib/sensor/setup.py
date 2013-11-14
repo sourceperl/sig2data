500ce3ebd60f07185789f77e6885bc1f56808715
sourceperl <loic.celine@free.fr>
2013-11-14 15:58:29 +0100

    add python setup.py, first version of beed = bee daemon

---
+++ lib/sensor/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='sensor',
+      version='1.0',
+      py_modules=['sensor'],
+      )
---
+++ python-daemon/lib/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='daemon3x',
+      version='1.0',
+      packages=['daemon3x'],
+      package_data={'daemon3x': ['examples/*.py']},
+      )
