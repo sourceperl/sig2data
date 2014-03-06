7be9766b3e338b6edda13499be345c512d0d3c26
sourceperl <loic.celine@free.fr>
2014-03-06 22:16:53 +0100

    update sensor.php: change SQL INSERT for msg duplicate bug workaround (provisional), update grant for SQL base

--- callback/sensor.php
+++ callback/sensor.php
@@ -1,14 +1,29 @@
 <?php
 // connect to DB
 $mysqli = new mysqli('localhost','bee_sigfox_cbk', '', 'beedb');
+
 // check DB connect
 if ($mysqli->connect_errno) {
-  printf("Connect failed: %s\n", $mysqli->connect_error);
-  http_response_code(500);
+  error_log('connect error: '.$mysqli->connect_error);
   exit();
 }
+
 // turn on auto-commit
 $mysqli->autocommit(TRUE);
+
+// define log function to SQL log
+function sql_log($message) {
+  // some vars
+  global $mysqli;
+  // log message to SQL DB
+  $sql = "INSERT INTO `sig_debug_log` (`id`, `date`, `text`) VALUES (NULL, ".
+         "NOW(), '".$message."');";
+  // check SQL query
+  if (! $mysqli->query($sql)) {
+    error_log('invalid SQL query: '.$mysqli->error());
+    exit();
+  }
+}
 
 // log client
 $stat_ip = $_SERVER['REMOTE_ADDR'];
@@ -20,28 +35,36 @@ $appid  = isset($_POST['appid'])    ? stripcslashes($_POST['appid'])    : "";
 $appkey = isset($_POST['appkey'])   ? stripcslashes($_POST['appkey'])   : "";
 
 // check the app key
-$result = $mysqli->query("SELECT id FROM `sig_vars` ".
-                         "WHERE var_name='callback_app_key' AND var='".
-                         $appkey."';");
+$sql = "SELECT id FROM `sig_vars` WHERE var_name='callback_app_key' ".
+       "AND var='".$appkey."';";
+$result = $mysqli->query($sql);
+
+// error check
+if (!$result) {
+  error_log("invalid SQL query: ".$mysqli->error());
+  exit();
+}
+
+// app_key mismatch
 if ($result->num_rows == 0) {
-  // log error and exit script
-  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) VALUES (NULL, ".
-                 "NOW(), 'err: app_key not valid, connect from IP: ".$stat_ip.
-                 "/".$ua."');");
-  $mysqli->close();
+  // log error and y3exit script
+  sql_log("error: app_key not valid, connect from IP: ".$stat_ip."/".$ua);
+  // log error and exit
+  error_log("error: app_key not valid", 0);
   http_response_code(500);
   exit();
 }
+// release result dataset
+$result->close();
 
 // decode JSON
 $data = json_decode($cb_msg);
 // check json decoding
 if ($data === null) {
-  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
-                 "VALUES (NULL, NOW(), 'err: json_decode return null json: ".
-                 $cb_msg."');");
-  $mysqli->close();
-  http_response_code(500); 
+  // log error and y3exit script
+  sql_log("error: json_decode return null json: ".$cb_msg);
+  // release result dataset
+  error_log("error: json_decode return null");
   exit();
 } else {
   $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
@@ -53,7 +76,7 @@ if ($data === null) {
 $uid         = hexdec($data->{'msg'}->{'ctxt'}->{'uid'});
 $timestamp   = isset($data->{'msg'}->{'received'})
                ? $data->{'msg'}->{'received'}/1000 : "";
-$msg_type    = isset($data->{'msg'}->{'type'}) 
+$msg_type    = isset($data->{'msg'}->{'type'})
                ? $data->{'msg'}->{'type'}          : "";
 $msg_payload = isset($data->{'msg'}->{'payload'})
                ? $data->{'msg'}->{'payload'}       : "";
@@ -67,13 +90,17 @@ $result = $mysqli->query("SELECT object_id FROM `sig_objects` WHERE modem_id='".
                           $uid."';");
 if ($result->num_rows == 0) {
   // log error and exit script
-  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) ".
-                 "VALUES (NULL, NOW(), 'err: unknown object uid: ".$uid."');");
-  $mysqli->close();
+  sql_log("error: unknown object UID: ".$uid);
+  // release result dataset
+  error_log("error: unknown object UID");
   exit();
 }
+
 $row = $result->fetch_assoc();
 $obj_id = $row['object_id'];
+
+// release result dataset
+$result->close();
 
 // insert in DB, if not already exist (if polling value or duplicate callback)
 // add 06/03/2014, duplicate bug workaround (before UNB modem firmware change):
@@ -88,23 +115,20 @@ $sql = "INSERT INTO `sig_messages` (`message_id`, `object_id`, "
        ."(`rx_timestamp` BETWEEN '".($timestamp - 600)."' AND '"
        .($timestamp + 600)."' AND `payload` = '".$msg_payload."'))"
        ." LIMIT 1);";
-/* old request
-$sql = "INSERT INTO `sig_messages` (`message_id`, `object_id`, "
-       ."`rx_timestamp`, `type`, `payload`, `station_id`, `station_lvl`) "
-       ."SELECT NULL, '".$obj_id."', '".$timestamp."', '".$msg_type."', '"
-       .$msg_payload."', '".$station_id."', '".$station_lvl."' FROM DUAL "
-       ."WHERE NOT EXISTS (SELECT * FROM `sig_messages` WHERE `object_id` = '"
-       .$obj_id."' AND `rx_timestamp`= '".$timestamp."' LIMIT 1);";
-*/
-$mysqli->query($sql);
+// SQL query error ?
+if (! $mysqli->query($sql)) {
+  error_log("SQL query error: ".$mysqli->error, 0);
+  exit();
 // check if insert or not (= duplicate)
-if ($mysqli->affected_rows == 0) {
+} else if ($mysqli->affected_rows == 0) {
   // log warning
-  $mysqli->query("INSERT INTO `sig_debug_log` (`id`, `date`, `text`) "
-                 ."VALUES (NULL, NOW(), "
-                 ."'warn: callback duplicate obj_id/timestamp: "
-                 .$obj_id."/".$timestamp."');");
+  sql_log("warning: callback duplicate object/timestamp: ".
+          $obj_id."/".$timestamp);
+  exit();
 }
+
+sql_log("log: store msg object_id ".$obj_id."/payload ".$msg_payload);
+
 $mysqli->close();
-exit()
+exit();
 ?>
--- sql/callback-user-grant.sql
+++ sql/callback-user-grant.sql
@@ -1,9 +1,9 @@
 GRANT USAGE ON *.* TO 'bee_sigfox_cbk'@'localhost';
 
-GRANT SELECT ON `beedb`.`vars` TO 'bee_sigfox_cbk'@'localhost';
+GRANT SELECT ON `beedb`.`sig_vars` TO 'bee_sigfox_cbk'@'localhost';
 
-GRANT INSERT ON `beedb`.`debug_log` TO 'bee_sigfox_cbk'@'localhost';
+GRANT INSERT ON `beedb`.`sig_debug_log` TO 'bee_sigfox_cbk'@'localhost';
 
-GRANT SELECT (rx_timestamp, message_id, object_id), INSERT ON `beedb`.`messages` TO 'bee_sigfox_cbk'@'localhost';
+GRANT SELECT (rx_timestamp, message_id, object_id, payload), INSERT ON `beedb`.`sig_messages` TO 'bee_sigfox_cbk'@'localhost';
 
-GRANT SELECT (modem_id, object_id) ON `beedb`.`objects` TO 'bee_sigfox_cbk'@'localhost';
+GRANT SELECT (modem_id, object_id) ON `beedb`.`sig_objects` TO 'bee_sigfox_cbk'@'localhost';
